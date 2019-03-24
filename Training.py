c2ca9a5538e0c641d6fcc8ca3012a363dbdd8e33
Elias <marks.eliasariel@gmail.com>
2019-03-24 19:03:45 +0100

    try

--- Training.py
+++ Training.py
@@ -89,7 +89,7 @@ def urmp():
     print("Training multi-instrument separation with URMP dataset")
     model_config = {
         "dataset_name": "urmp",
-        "data_path": "ris",
+        "data_path": "gs://vimsstfrecords/urmp-labels",
         "estimates_path": "estimates",
         "model_base_dir": "gs://modelcheckpoints", # Base folder for model checkpoints
         "output_type": "difference",
